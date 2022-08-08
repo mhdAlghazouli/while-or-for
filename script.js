23a0646a09220585390a5f1738690b8d93925835
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-08 13:29:24 -0400

    Added some exercises to script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ for (let i = toyota.length ; i > 0  ; i--) {
   toyota.pop();
 
   console.log(toyota);
-  
+
 }
 
-// console out put : [ 'corolla', 'camry', 'rav-4', 'tacoma', 'tundra' ]
+// console output : [ 'corolla', 'camry', 'rav-4', 'tacoma', 'tundra' ]
                       // [ 'corolla', 'camry', 'rav-4', 'tacoma' ]
                             // [ 'corolla', 'camry', 'rav-4' ]
                                   // [ 'corolla', 'camry' ]
@@ -33,9 +33,87 @@ while(counter > 0){
   
 };
 
-// console out put : [ 'corolla', 'camry', 'rav-4', 'tacoma', 'tundra' ]
+// console output : [ 'corolla', 'camry', 'rav-4', 'tacoma', 'tundra' ]
                       // [ 'corolla', 'camry', 'rav-4', 'tacoma' ]
                             // [ 'corolla', 'camry', 'rav-4' ]
                                   // [ 'corolla', 'camry' ]
                                         //  [ 'corolla' ]
                                               //  []
+
+// Function `printNumbers()` prints numbers from 1 to 10 using for loop.
+
+function printNumbers() {
+      for(let i = 1 ; i <=10 ; i++){
+            console.log(i);
+      };
+};
+printNumbers(); //1 2 3 4 5 6 7 8 9 10
+
+
+// Function `printArray()` prints all the elements of a 2D array using nested for loops.
+
+let arr1 = [[1, 2],[3, 4],[5, 6]];
+
+function printArray(array){
+      for(let i = 0 ; i < array.length; i++){
+            let char = array[i];
+            for(let j = 0 ; j < char.length; j++){
+                  console.log(char[j]);
+            };
+      };
+};
+printArray(arr1); //1 2 3 4 5 6
+
+// Function `printEven()` prints all the even numbers of a 2D array using for loops and ‘%’ operator.
+
+let arr2 = [13,23,12,45,22,48,66,100];
+
+function printEven (array) {
+      for(let i = 0 ; i < array.length; i++){
+            let char = array[i];
+            if(char % 2 === 0){
+                  console.log(char);
+            };
+      };
+};
+
+printEven(arr2); //12 22 48 66 100
+
+// Function `deleteElement()` deletes all the occurrence of element from the given array.
+
+
+
+let arr3 = [23,56,4,78,5,63,45,210,56];
+
+function deleteElement(array,target) {
+      for(let i = 0 ; i < array.length ; i++){
+            let char = array[i];
+            if(char === target) {
+                  
+                  array.splice(i,1);
+
+            };
+      };
+      
+      return array
+};
+
+
+console.log(deleteElement(arr3,56)); //[23, 4, 78, 5, 63, 45, 210]
+
+// Function numPower() to returns power of number for provided exponential value using for loop.
+
+function numPower(num1,num2){
+
+      let result = 1;
+
+      for(let i = 0 ; i < num2; i++){
+            
+            result = result * num1;
+            
+      };
+      return result
+};
+
+console.log(numPower(4,3)); //64
+console.log(numPower(16,2)); //256
